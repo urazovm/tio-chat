7780c5d5641d6a9faea4ec88d26d34eeedf57c1d
Taran <zealenth.gmail.com>
2016-06-21 22:50:30 -0400

    removing worker tasks

--- worker.js
+++ worker.js
@@ -5,20 +5,12 @@ mongoose.connect(dbURI);
 
 var db = mongoose.connection;
 
-const periodic = require('./server/worker/periodic');
-const rankings = require('./server/worker/rankings');
 const User = require('./server/db/user').getUserModel(mongoose);
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
   console.log( 'Worker connected to MongoDB' );
 
-  setInterval(()=>{
-    rankings.updateRankings(mongoose);
-  }, 1000*60*10);
-
-  periodic.setupPeriodicTasks(mongoose);
-  rankings.updateRankings(mongoose);
 });
 
 
