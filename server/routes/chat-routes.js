abecb225dd1c18f554a1132855c41dc9687b6279
trorem <trorem@factset.com>
2016-06-22 18:10:31 -0400

    users

--- server/routes/chat-routes.js
+++ server/routes/chat-routes.js
@@ -3,6 +3,15 @@ const _ = require('lodash');
 
 const ChatMsgModel = require('../db/chat-msg').ChatMsg;
 
+function addRoomToSocket(socket, room) {
+  if (!socket.tioRooms) {
+    socket.tioRooms = [];
+  }
+  if (_.findIndex(socket.tioRooms, room) === -1) {
+    socket.tioRooms.push(room);
+  }
+}
+
 function addChatRoutes(app, io) {
   io.on('authenticated', (socket)=> {
     socket.on('rooms:init', (rooms) => {
@@ -16,18 +25,30 @@ function addChatRoutes(app, io) {
 
     socket.on('chat:join', (room) => {
       console.log('joined: ' + room);
-      socket.join(room);
+
+      addRoomToSocket(socket, room);
+
+
+
       ChatMsgModel.find({roomId: room}).exec()
         .then((docs)=> {
           socket.emit('chat:init', {roomId: room, msgs: docs} )
         });
+      /* TODO: this example code might work?
+      let roomUsers = [];
+       const clients_in_the_room = io.sockets.adapter.rooms[room];
+       for (var clientId in clients_in_the_room ) {
+       console.log('client: %s', clientId); //Seeing is believing
+       var client_socket = io.sockets.connected[clientId];//Do whatever you want with this
+       }
+       */
+      socket.join(room);
+      io.to(room).emit('user:join', socket.decoded_token.user);
     });
 
     socket.on('chat:add', (msg)=>{
-      console.log('new message');
       msg.fromUser = socket.decoded_token.user;
       let chat = new ChatMsgModel(msg);
-      console.log(chat);
       chat.save()
         .then(()=>{
           io.to(msg.roomId).emit('chat:add', msg);
@@ -36,7 +57,26 @@ function addChatRoutes(app, io) {
 
     socket.on('chat:leave', (room)=> {
       socket.leave(room);
+      if(socket.tioRooms) {
+        socket.tioRooms = _.filter(socket.tioRooms, (socketRoom) =>{
+          return socketRoom !== room
+        });
+      }
+      io.to(room).emit('user:leave', socket.decoded_token.user);
     });
+
+    socket.on('disconnect', () => {
+      _.each(socket.tioRooms, (room) => {
+        io.to(room).emit('user:leave', socket.decoded_token.user);
+      });
+    });
+
+    socket.on('reconnect', ()=> {
+      _.each(socket.tioRooms, (room) => {
+        io.to(room).emit('user:join', socket.decoded_token.user);
+      });
+    });
+
 
     ChatMsgModel.find()
       .distinct('roomId')
