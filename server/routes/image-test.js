70c328f0c7d0ee6cb98de13e74ce951d64dae4c5
Taran <zealenth.gmail.com>
2016-07-31 22:20:41 -0400

    randomly trying stuff

--- server/routes/image-test.js
+++ server/routes/image-test.js
@@ -104,7 +104,8 @@ module.exports.setupImageRoutes = (app) => {
   });
 
   app.get('/image', (req, resp)=> {
-    req.pipe(requestCB(req.query.url)).pipe(resp);
+    let url = req.query.url.replace( /^https/i, 'http');
+    requestCB(url).pipe(resp);
     //request.get({url: req.query.url, headers: {"cache-control": "none"}}).pipe(resp);
   });
 };
